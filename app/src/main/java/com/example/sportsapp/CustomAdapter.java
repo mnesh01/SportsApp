df50f60af214b26c9a388e40e2f9f90f15cccd88
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-08-12 15:32:26 +0300

    Add SportsViewHolder

--- app/src/main/java/com/example/sportsapp/CustomAdapter.java
+++ app/src/main/java/com/example/sportsapp/CustomAdapter.java
@@ -3,13 +3,16 @@ package com.example.sportsapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class CustomAdapter extends RecyclerView.Adapter<> {
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.SportsViewHolder> {
+    // added SportViewHolder class as the type parameter
     // custom adapter acts as a bridge between data(list) and UI(recyclerview and cardview)
 
     private List<Sport> sportList;
@@ -25,7 +28,7 @@ public class CustomAdapter extends RecyclerView.Adapter<> {
     @NonNull
     @Override
 
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public SportsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //inflates layout for each item in the recyclerView
         View itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.card_item_layout,
@@ -36,22 +39,38 @@ public class CustomAdapter extends RecyclerView.Adapter<> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SportsViewHolder holder, int position) {
+        // called for each item in the list
+        // binding data from sport object to views within the SportsViewHolder
+        // always use the custom viewholder
+
+        Sport sport = sportList.get(position);
+        holder.textView.setText(sport.sportName);
+        holder.imageView.setImageResource(sport.sportImg);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        // how many items to display
+        return sportList.size();
     }
 
     public static class SportsViewHolder extends RecyclerView.ViewHolder{
         // SportsViewHolder holds the references to the views within the item layout allowing efficient recycling of views
 
+        TextView textView;
+        ImageView imageView;
 
 
+        public SportsViewHolder(@NonNull View itemView) {
+            super(itemView);
 
+            textView = itemView.findViewById(R.id.textView);
+            imageView = itemView.findViewById(R.id.imageviewcard);
 
+        }
     }
 
 }
