dd4d423017c292ed6cd74dfb2c990486b1d4f2a6
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-08-12 15:44:19 +0300

    Set RecyclerView to display sports with CustomAdapter

--- app/src/main/java/com/example/sportsapp/MainActivity.java
+++ app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -7,18 +7,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // 1 AdapterView
+    private RecyclerView recyclerView;
+
+    // 2 Data source
+    private List<Sport> sportList;
+
+    // 3. Adapter
+    private CustomAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("VolleyBall", R.drawable.volley);
+        Sport s4 = new Sport("Tennis", R.drawable.tennis);
+        Sport s5 = new Sport("Ping Pong", R.drawable.ping);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+        myAdapter = new CustomAdapter(sportList);
+
+        //use layout manager to manage the layout of its items. Arranged linearly
+        recyclerView.setLayoutManager(new LinearLayoutManager(this
+        ));
+        //recycler view is the reference of recyclerview instance
+        //setlayoutmanager for the recyclerview
+        // new - creates a new instance
+        // this- reference of the context
+        recyclerView.setAdapter(myAdapter);
+
+
+        }
     }
-}
